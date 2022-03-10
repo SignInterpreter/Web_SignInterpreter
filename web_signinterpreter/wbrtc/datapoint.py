5c9da7ae63496126c97ac39afe9e6903c0c8dac6
BorjaEACode <espesborja@gmail.com>
2022-03-10 16:18:45 +0100

    arreglos

--- web_signinterpreter/wbrtc/datapoint.py
+++ web_signinterpreter/wbrtc/datapoint.py
@@ -2,31 +2,36 @@ from data.keypoints import mp_holistic,mediapipe_detection, draw_styled_landmark
 from data.datarecord import actions
 from data.values import extract_keypoints
 from pronosticador.predict import  predice, extraerkeypoints
-
+import asyncio
 
 global sequence2 
 sequence2 = []
 
+#originally the model was trained in order to recover 30 frames and create predicction witrh movement
+#as the time that we have for the project is only 2 weeks 
+#we have take the decision of use an static image to recreate the necesary shape for the model.
+#this is the reason why the sequence2 add 30 times the same keypoint.
+
 
 def get_datapoint(frame):
-
+    
     with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-        global sequence2                
+        global sequence2 
+        keypoint=  extraerkeypoints(frame,holistic)                 
         while len(sequence2)!=30:
-            print(len(sequence2))
-            sequence2.append(extraerkeypoints(frame,holistic))
-
-        sequence2 = sequence2[-30:]
-        print("secuence2 "+ str(len(sequence2)) )
-        
-        return sequence2
+            sequence2.append(keypoint)       
+        sequence2 = sequence2[-30:]                
+    return sequence2
 
 
 def get_pronostico(sequence):
-    sentence= "getting info"
+    
     if len(sequence) == 30: 
         global sequence2 
         sequence2 = []                   
         sentence = predice(sequence)
+        if  sentence:           
+            sentence =str(sentence[0].replace("['","").replace("']",""))
+        
         print("Running inference..."+str(sentence))
         return str(sentence)
